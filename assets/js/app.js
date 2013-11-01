92a720183ffbcf426af8d03707b948a0b0c3a532
Lucas <lucas@agtlucas.com>
2013-11-01 10:10:05 -0200

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,23 @@
 'use strict';
 
+// Define Angular Module
 var app = angular.module('noMoreLies', []);
 
-app.controller('MainCtrl', function($scope, $timeout, Weather) {
+// Main Controller
+app.controller('MainCtrl', function($scope, $timeout, Weather, UserService) {
     // Date object
     $scope.date = {};
-
     // Update function
     var updateTime = function() {
-        $scope.date.raw = new Date();
+        $scope.date.tz = new Date(new Date().toLocaleString(
+                'en-US', {timezone: $scope.user.timezone}
+            ));
         $timeout(updateTime, 1000);
     }
 
     $scope.weather = {}
-    Weather.getWeatherForecast('CA/San_Francisco')
+    $scope.user = UserService.user;
+    Weather.getWeatherForecast($scope.user.location)
     .then(function(data) {
         $scope.weather.forecast = data;
     });
@@ -22,7 +26,38 @@ app.controller('MainCtrl', function($scope, $timeout, Weather) {
     updateTime();
 });
 
+// Setting Controller
+app.controller('SettingsCtrl', function($scope, UserService, Weather) {
+    $scope.user = UserService.user;
+    $scope.fetchCities = Weather.getCityDetails;
 
+    $scope.save = function() {
+        UserService.save();
+    }
+});
+
+// Factory
+app.factory('UserService', function() {
+    var defaults = {
+        location: 'autoip'
+    };
+    var service = {
+        user: {},
+        save: function() {
+            sessionStorage.presently = angular.toJson(service.user);
+        },
+        restore: function() {
+            service.user = angular.fromJson(sessionStorage.presently) || defaults
+
+            return service.user;
+        }
+    };
+
+    service.restore();
+    return service;
+});
+
+// Provider
 app.provider('Weather', function() {
     var apiKey = "";
 
@@ -51,11 +86,100 @@ app.provider('Weather', function() {
                     d.reject(err);
                 });
                 return d.promise;
+            },
+
+            getCityDetails: function(query) {
+                var d = $q.defer();
+                $http({
+                    method: 'GET',
+                    url: 'http://autocomplete.wunderground.com/' +
+                         'aq?query=' +
+                         query
+                }).success(function(data) {
+                    d.resolve(data.RESULTS);
+                }).error(function(err) {
+                    d.reject(err);
+                });
+                return d.promise;
             }
         }
     }
 });
 
+// Directives
+app.directive('autoFill', function($timeout) {
+    return {
+        restrict: 'EA',
+        scope: {
+            autoFill: '&',
+            ngModel: '='
+        },
+        compile: function(tEle, tAttrs) {
+            // Compile function
+            var tplEl = angular.element(
+                '<div class="typehead">' +
+                    '<input type="text" autocomplete="off" />' +
+                        '<ul class="auto-list" ng-show="reslist">' +
+                            '<li ng-repeat="res in reslist" ' +
+                            '>{{res.name}}</li>' +
+                        '</ul>' +
+                '</div>'
+            );
+
+            var input = tplEl.find('input');
+            input.attr('type', tAttrs.type);
+            input.attr('ng-model', tAttrs.ngModel);
+            input.attr('timezone', tAttrs.timezone);
+            tEle.replaceWith(tplEl);
+            return function(scope, ele, attrs, ctrl) {
+                var minKeyCount = attrs.minKeyCount || 3,
+                    timer,
+                    val,
+                    input = ele.find('input');
+
+                input.bind('keyup', function(e) {
+                    val = ele.val();
+                    if (val.length < minKeyCount) {
+                        if (timer) $timeout.cancel(timer);
+                        scope.reslist = null;
+                        return;
+                    } else {
+                        if (timer) $timeout.cancel(timer);
+                        timer = $timeout(function() {
+                            scope.autoFill()(val)
+                            .then(function(data) {
+                                if (data && data.length > 0) {
+                                    scope.reslist = data;
+                                    scope.ngModel = 'zmw:' + data[0].zmw;
+                                    scope.timezone = data[0].tz;
+                                }
+                            });
+                        }, 300);
+                    }
+                });
+
+                input.bind('blur', function(e) {
+                    scope.reslist = null;
+                    scope.$digest();
+                });
+            }
+        }
+    }
+});
+
+// Configs
 app.config(function(WeatherProvider) {
     WeatherProvider.setApiKey('0bf91bb915508618');
+});
+
+app.config(function($routeProvider) {
+    $routeProvider.when('/', {
+        templateUrl: 'templates/home.html',
+        controller: 'MainCtrl'
+    })
+    .when('/settings', {
+        templateUrl: 'templates/settings.html',
+        controller: 'SettingsCtrl'
+    })
+    .otherwise({redirectTo: '/'});
 });
